59575c7e5cfd2c6edefa4e8b6f04039afd27b4ca
Parikshat Singh <79864725+Parikshatsingh1999@users.noreply.github.com>
2024-01-01 19:54:00 +0530

    removed cors congigs

--- index.js
+++ index.js
@@ -8,23 +8,12 @@ import { router } from "./router/index.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const options = [
-    cors({
-        origin: '*',
-        methods: '*',
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true,
-    })
-];
-
-app.use(options);
 
 
 app.use("/api", router)
