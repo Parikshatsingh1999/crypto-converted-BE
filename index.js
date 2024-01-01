785391aa4bf0baf9693d5450431b86ea3843f091
Parikshat Singh <79864725+Parikshatsingh1999@users.noreply.github.com>
2024-01-01 13:38:00 +0530

    added whitelist in cors

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+    origin: ['https://crypto-converter-fe.vercel.app']
+}))
 
 
 app.use("/api", router)
