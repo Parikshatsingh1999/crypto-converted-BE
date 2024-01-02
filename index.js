32f4f3b03606a688229fd6d92c12ff6752612e45
Parikshat Singh <79864725+Parikshatsingh1999@users.noreply.github.com>
2024-01-02 16:33:59 +0530

    added headers middlesware

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: ['https://crypto-converter-fe.vercel.app', 'http://localhost:3000']
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 const PORT = process.env.PORT || 5000;
 
